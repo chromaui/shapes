ba40caf73f805b6ea0be03129e80f438a69e5ae3
Gert Hengeveld <info@ghengeveld.nl>
2019-10-14 15:55:12 +0200

    Snapshot the Grid at two viewport sizes.

--- stories/Grid.stories.js
+++ stories/Grid.stories.js
@@ -2,7 +2,10 @@ import React from "react"
 import Grid from "../screens/Grid"
 
 export default {
-  title: "screens|Shapes/Grid"
+  title: "screens|Shapes/Grid",
+  parameters: {
+    chromatic: { viewports: [250, 750] }
+  }
 }
 
 export const threeXTwo = () => <Grid />
