b21036006fbcd9a01823b8f7f438ed14a6373539
Gert Hengeveld <info@ghengeveld.nl>
2019-12-11 00:24:58 +0100

    Set body styles based on story kind.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,25 @@
-import { addParameters, configure } from '@storybook/react';
+import React from 'react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
+import { Global, css } from '@emotion/core';
+
+addDecorator((storyFn, { kind, parameters }) => {
+  const type = kind.split(parameters.options.hierarchyRootSeparator)[0];
+  const isScreen = type === 'screens';
+  return (
+    <>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+            background: ${isScreen ? '#f6f9fc' : '#fff'};
+            padding: ${isScreen ? 0 : 1}rem;
+          }
+        `}
+      />
+      {storyFn()}
+    </>
+  );
+});
 
 addParameters({
   options: {
