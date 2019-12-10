c0451b654f5723de69b342955bc8cd0a7435f036
Gert Hengeveld <info@ghengeveld.nl>
2019-12-11 00:58:54 +0100

    Use a wrapper component instead of body/root styles.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,24 +1,24 @@
+/** @jsx jsx */
 import React from 'react';
 import { addDecorator, addParameters, configure } from '@storybook/react';
-import { Global, css } from '@emotion/core';
+import { Global, css, jsx } from '@emotion/core';
 
 addDecorator((storyFn, { kind, parameters }) => {
   const [storyRoot] = kind.split(parameters.options.hierarchyRootSeparator);
   const isScreen = storyRoot === 'screens';
+  const globalStyles = css`
+    body {
+      margin: 0;
+      background: ${isScreen ? '#f6f9fc' : '#fff'};
+    }
+  `;
+  const wrapperStyles = css`
+    padding: ${isScreen ? 0 : 1}rem;
+  `;
   return (
     <>
-      <Global
-        styles={css`
-          body {
-            margin: 0;
-            background: ${isScreen ? '#f6f9fc' : '#fff'};
-          }
-          #root {
-            padding: ${isScreen ? 0 : 1}rem;
-          }
-        `}
-      />
-      {storyFn()}
+      <Global styles={globalStyles} />
+      <div css={wrapperStyles}>{storyFn()}</div>
     </>
   );
 });
