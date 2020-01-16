d07b4fe9da6a3d6cfdb7f6097806b8852b925491
Gert Hengeveld <info@ghengeveld.nl>
2020-01-16 11:51:11 +0100

    Actually change something as well.

--- src/components/Shapes.tsx
+++ src/components/Shapes.tsx
@@ -26,7 +26,7 @@ export const L = (props: Props) => (
 );
 
 export const Slice = (props: Props) => (
-  <Shape viewBox="0 0 92 92" xmlns="http://www.w3.org/2000/svg" color="#FF4785" {...props}>
+  <Shape viewBox="0 0 92 92" xmlns="http://www.w3.org/2000/svg" color="#1EA7FD" {...props}>
     <path d="M92 89V3c0-3-2.056-3-3-3C39.294 0 0 39.294 0 89c0 0 0 3 3 3h86a3 3 0 003-3z" />
   </Shape>
 );
