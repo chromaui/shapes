36aabd1c3f2d50f04199926bb888f609f0c91065
Gert Hengeveld <info@ghengeveld.nl>
2020-02-13 16:14:35 +0100

    Reshuffle again.

--- src/screens/Composition.tsx
+++ src/screens/Composition.tsx
@@ -20,12 +20,12 @@ const Composition = styled.div({
 export default () => (
   <Container>
     <Composition>
-      <Donut style={{ left: 380, top: 250, height: 120 }}></Donut>
-      <L style={{ left: 40, top: 0, height: 120 }}></L>
-      <Slice style={{ left: 440, top: 20, height: 120 }}></Slice>
-      <Square style={{ left: 0, top: 280, height: 80 }}></Square>
+      <Slice style={{ left: 400, top: 240 }}></Slice>
+      <Z style={{ left: 20, top: 0, height: 120 }}></Z>
+      <Donut style={{ left: 440, top: 20 }}></Donut>
+      <Square style={{ left: 0, top: 250, height: 90 }}></Square>
       <Triangle style={{ left: 200, top: 70, height: 120 }}></Triangle>
-      <Z style={{ left: 200, top: 330, height: 120 }}></Z>
+      <L style={{ left: 220, top: 330, height: 120 }}></L>
     </Composition>
   </Container>
 );
