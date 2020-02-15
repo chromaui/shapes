c6c21ca9bbaf9d1f98aafe466c9d75d1ab50c2ba
Gert Hengeveld <info@ghengeveld.nl>
2020-02-15 21:33:32 +0100

    Dark paper.

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -1,18 +1,18 @@
 import styled from '@emotion/styled';
-import { color } from '../tokens/palette';
 
 interface Props {
   interactive: boolean;
 }
 
 export default styled.div<Partial<Props>>(({ interactive = false }) => ({
-  background: color.lightest,
+  background: '#232e38',
   boxShadow: '0 1px 3px rgba(0, 0, 0, 0.05)',
   borderRadius: 5,
   transition: 'all 150ms ease-out',
   transform: 'translate3d(0, 0, 0)',
   overflow: 'auto',
   verticalAlign: 'top',
+  color: '#eff2f5',
 
   ...(interactive && {
     cursor: 'default',
