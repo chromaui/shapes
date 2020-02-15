55b2faee366b427ffc427c0a9557af157ba2621c
Gert Hengeveld <info@ghengeveld.nl>
2020-02-15 22:10:26 +0100

    Dark tile.

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -4,7 +4,7 @@ export default styled.div({
   display: 'inline-flex',
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundImage: 'linear-gradient(0deg, #f8f8f8f7 0%, #fffffff7 100%)',
+  backgroundImage: 'linear-gradient(0deg, #232e38 0%, #3f5a73 100%)',
   boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.06)',
   borderRadius: 3,
   padding: '1rem',
