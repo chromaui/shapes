9d96c51baf2c48efedfed6cd1b976527b0702daa
Gert Hengeveld <info@ghengeveld.nl>
2021-04-12 21:26:49 +0200

    Bump tile padding

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -7,6 +7,6 @@ export default styled.div({
   backgroundImage: 'linear-gradient(0deg, #f8f8f8f7 0%, #fffffff7 100%)',
   boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.06)',
   borderRadius: 3,
-  padding: '1rem',
+  padding: '2rem',
   verticalAlign: 'top',
 });
