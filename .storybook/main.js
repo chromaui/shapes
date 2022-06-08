dab6ff7aa3511999e522f93b1708491361416978
Jono Kolnik <1164060+JonathanKolnik@users.noreply.github.com>
2022-06-08 10:59:42 -0400

    blue donut

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+ refs: {
+   'design-system': {
+     title: "Storybook Design System",
+     url: "https://master--628697577545d87bef27ee2f.dev-chromatic.com",
+   }
+  }
 };
